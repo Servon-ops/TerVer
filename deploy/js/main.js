67b0f849fd6c8e013ad6881e3a5c31558c882ed4
Safiullin Ayaz | 11-106 <57864136+Servon-ops@users.noreply.github.com>
2022-12-21 13:50:28 +0300

    Add files via upload
    ok

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -6,22 +6,38 @@ const mainState = {
     result: 0
 }
 
-const formulaState = {
-    fpb: "fpb",
-    fps: "fps",
-    frb: "frb",
-    frs: "frs",
-    fsb: "fsb",
-    fss: "fss",
-}
-
 const verTypesButton = document.querySelectorAll('.s-button, .p-button, .r-button')
 verTypesButton.forEach(btn => {
     btn.addEventListener('click', event => {
         verTypesButton.forEach(tag => tag.style.backgroundColor = 'white')
         btn.style.backgroundColor = 'lightblue'
         mainState.type = btn.className
+        if (mainState.type === 'p-button' && document.querySelector('.input-repeat').checked === false) {
+            document.querySelector('.input-k').style.display = 'none';
+        }
+        else {
+            document.querySelector('.input-k').style.display = 'inline-block';
+        }
     })
+})
+
+const checkboxDiv = document.querySelector('.check-box-div')
+checkboxDiv.addEventListener('click', event => {
+    if (mainState.type === '') {
+        alert('Сначала выберите закон')
+        document.querySelector('.input-repeat').checked = false
+    }
+    else {
+        if (mainState.type === 'p-button' && document.querySelector('.input-repeat').checked === false) {
+            document.querySelector('.input-k').style.display = 'none';
+        }
+        else {
+            document.querySelector('.input-k').style.display = 'inline-block';
+        }
+    }
+    if (mainState.type === 'p-button' && document.querySelector('.input-repeat').checked === true) {
+        document.querySelector('.input-k').type = "text"
+    }
 })
 
 const checkbox = document.querySelector('.input-repeat')
@@ -35,31 +51,47 @@ resultButton.addEventListener('click', event => {
 })
 
 
+
 const result = () => {
+    if (mainState.n > 0) {
+        alert('Неверные данные')
+    }
+    if (mainState.n === '' && mainState.k === '') {
+        alert('Пустые данные')
+    }
+    document.querySelectorAll('.formula').forEach(form => form.style.display = 'none')
     mainState.n = Number(mainState.n)
-    mainState.k = Number(mainState.k)
+    if (!(mainState.repeat === true && mainState.type === 'p-button')) {
+        mainState.k = Number(mainState.k)
+    }
     if (mainState.type === 'p-button') {
         if (mainState.repeat === false) {
             mainState.result = factorial(Number(mainState.n))
+            document.querySelector('.fpb').style.display = 'block'
         }
         else {
-            //fsdf
+            mainState.result = factorial(mainState.n) / proiz(mainState.k)
+            document.querySelector('.fps').style.display = 'block'
         }
     }
     if (mainState.type === 'r-button') {
         if (mainState.repeat === false) {
             mainState.result = factorial(mainState.n) / factorial(mainState.n - mainState.k)
+            document.querySelector('.frb').style.display = 'block'
         }
         else {
             mainState.result = Math.pow(mainState.n, mainState.k)
+            document.querySelector('.frs').style.display = 'block'
         }
     }
     if (mainState.type === 's-button') {
         if (mainState.repeat === false) {
             mainState.result = factorial(mainState.n) / (factorial(mainState.n - mainState.k) * factorial(mainState.k))
+            document.querySelector('.fsb').style.display = 'block'
         }
         else {
             mainState.result = factorial(mainState.n + mainState.k - 1) / (factorial(mainState.n - 1) * factorial(mainState.k))
+            document.querySelector('.fss').style.display = 'block'
         }
     }
 }
@@ -69,6 +101,16 @@ const factorial = (n) => {
     for (let i = 1; i <= n; i++) {
         res = i * res
     }
+    return res
+}
+
+const proiz = (k) => {
+    let res = 1;
+    k = k.toString().split(' ')
+    k.forEach(n => {
+        n = factorial(Number(n))
+        res = n * res
+    })
     return res
 }
 
